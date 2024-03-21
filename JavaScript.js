4601482edddbd71c1cca35c1983992820aa2334a
poli <xyz@gmail.com>
2024-03-21 07:11:04 +0530

    add two numbers

---
+++ JavaScript.js
@@ -0,0 +1,12 @@
+
+// const num1 = 5;
+// const num2 = 10;
+// const sum = num1 + num2;
+// console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' +sum);
+
+    //Numbers entered by the user
+const num1 = parseInt(prompt("Enter the num1"));
+const num2 = parseInt(prompt("Enter the num2"));
+const sum = num1 + num2;
+// console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
